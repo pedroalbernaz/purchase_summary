412742c51ff7f13827139fa8d7a58c4b3d048cf0
Pedro Martins Albernaz <pedromartinsalbernaz@Pedros-MacBook-Pro.local>
2019-03-26 10:44:13 -0700

    fixing module update issue

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,12 +1,9 @@
+import { createStore, compose, applyMiddleware } from "redux";
+import rootReducer from "../rootReducer";
+import thunk from "redux-thunk";
 
-import {createStore, applyMiddleware} from 'redux';
-import rootReducer from '../rootReducer';
-import thunk from 'redux-thunk';
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export default function configureStore() {
-  return createStore(
-    rootReducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-    applyMiddleware(thunk)
-  );
+  return createStore(rootReducer, composeEnhancer(applyMiddleware(thunk)));
 }
